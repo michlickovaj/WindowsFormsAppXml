48ef3c518ab3b6b375c2f6ae080db85d9806e125
michlickovaj <michlickovaj@seznam.cz>
2024-02-24 09:17:18 +0100

    design

---
+++ WindowsFormsAppXml/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace WindowsFormsAppXml
+{
+    partial class xmlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(xmlForm));
+            this.menuToolStrip = new System.Windows.Forms.ToolStrip();
+            this.otevritToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.ulozitToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.zavritToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.menuToolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuToolStrip
+            // 
+            this.menuToolStrip.BackColor = System.Drawing.SystemColors.Control;
+            this.menuToolStrip.GripMargin = new System.Windows.Forms.Padding(10);
+            this.menuToolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.menuToolStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.otevritToolStripButton,
+            this.ulozitToolStripButton,
+            this.zavritToolStripButton});
+            this.menuToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuToolStrip.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.menuToolStrip.Name = "menuToolStrip";
+            this.menuToolStrip.Size = new System.Drawing.Size(800, 38);
+            this.menuToolStrip.TabIndex = 0;
+            this.menuToolStrip.Text = "toolStrip1";
+            // 
+            // otevritToolStripButton
+            // 
+            this.otevritToolStripButton.AutoSize = false;
+            this.otevritToolStripButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.otevritToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.otevritToolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.otevritToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("otevritToolStripButton.Image")));
+            this.otevritToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.otevritToolStripButton.Name = "otevritToolStripButton";
+            this.otevritToolStripButton.Size = new System.Drawing.Size(76, 29);
+            this.otevritToolStripButton.Text = "Otevřít";
+            this.otevritToolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.otevritToolStripButton.Click += new System.EventHandler(this.otevritToolStripButton_Click);
+            // 
+            // ulozitToolStripButton
+            // 
+            this.ulozitToolStripButton.AutoSize = false;
+            this.ulozitToolStripButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ulozitToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ulozitToolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ulozitToolStripButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ulozitToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("ulozitToolStripButton.Image")));
+            this.ulozitToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ulozitToolStripButton.Margin = new System.Windows.Forms.Padding(10, 2, 0, 3);
+            this.ulozitToolStripButton.Name = "ulozitToolStripButton";
+            this.ulozitToolStripButton.Padding = new System.Windows.Forms.Padding(5);
+            this.ulozitToolStripButton.Size = new System.Drawing.Size(76, 29);
+            this.ulozitToolStripButton.Text = "Uložit";
+            // 
+            // zavritToolStripButton
+            // 
+            this.zavritToolStripButton.AutoSize = false;
+            this.zavritToolStripButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.zavritToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.zavritToolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.zavritToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("zavritToolStripButton.Image")));
+            this.zavritToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.zavritToolStripButton.Margin = new System.Windows.Forms.Padding(10, 2, 0, 3);
+            this.zavritToolStripButton.Name = "zavritToolStripButton";
+            this.zavritToolStripButton.Padding = new System.Windows.Forms.Padding(5);
+            this.zavritToolStripButton.Size = new System.Drawing.Size(76, 29);
+            this.zavritToolStripButton.Text = "Zavřít";
+            this.zavritToolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 60);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(372, 360);
+            this.treeView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(402, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Název souboru:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(640, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(402, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hloubka nejzanoř.elemetu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(640, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(402, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(192, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Maximální počet potomků:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(640, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(402, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(180, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Minimální počet atributů:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(640, 153);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(402, 187);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(184, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Maximální počet atributů:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(640, 187);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 20);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.Location = new System.Drawing.Point(460, 13);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(216, 25);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Informace o souboru:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label12.Location = new System.Drawing.Point(465, 230);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(226, 25);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Informace o elementu:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(402, 266);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(137, 20);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "Hloubka zanoření:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(640, 266);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(60, 20);
+            this.label14.TabIndex = 15;
+            this.label14.Text = "label14";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(402, 296);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(176, 20);
+            this.label15.TabIndex = 16;
+            this.label15.Text = "Pořadí mezi sourozenci:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(640, 296);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(60, 20);
+            this.label16.TabIndex = 17;
+            this.label16.Text = "label16";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(402, 326);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(187, 20);
+            this.label17.TabIndex = 18;
+            this.label17.Text = "Názvy a hodnoty atributů:";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(640, 326);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(60, 20);
+            this.label18.TabIndex = 19;
+            this.label18.Text = "label18";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(402, 375);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(43, 20);
+            this.label19.TabIndex = 20;
+            this.label19.Text = "Text:";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(640, 375);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(60, 20);
+            this.label20.TabIndex = 21;
+            this.label20.Text = "label20";
+            // 
+            // xmlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuToolStrip);
+            this.Name = "xmlForm";
+            this.Text = "Správce XML souborů";
+            this.menuToolStrip.ResumeLayout(false);
+            this.menuToolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip menuToolStrip;
+        private System.Windows.Forms.ToolStripButton otevritToolStripButton;
+        private System.Windows.Forms.ToolStripButton ulozitToolStripButton;
+        private System.Windows.Forms.ToolStripButton zavritToolStripButton;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+    }
+}
+
